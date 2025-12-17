0be7601dcbc7653cb739d1e49d59583936cc5d7c
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-12-17 08:25:20 -0300

    commit na manhã

--- tasks/poo/zoo/py/draft.py
+++ tasks/poo/zoo/py/draft.py
@@ -1,6 +1,33 @@
 from abc import ABC, abstractmethod
+
 class animal (ABC):
-    @abstractmethod
- def apresentarNome(self):
-    pass
- def ""
+ def __init__(self, nome: str):
+   self.__nome: str = nome
+
+ def apresentarNome(self) -> str:
+    print(f"Eu sou um(a) {self.__nome}!")
+
+ @abstractmethod
+ def fazerSom(self):
+   pass
+ 
+ @abstractmethod
+ def mover(self):
+   pass
+ 
+class Leão(Animal):
+   def __init__(self, nome: str):
+    super().__init__(nome)
+
+   def fazerSom(self):
+     print(f"roawwwwww")
+
+   def mover(self):
+     print(f"azunhão")
+
+   class Elefante(animal):
+     def__init__(self, nome: str):
+     super().__init__(nome)
+     def fazerSom(self):
+       print(f"")
+ 
